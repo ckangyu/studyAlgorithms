e95b1cd009a69fe4b736e9d4840427bde30b2f69
ckangyu <wl03040452@gmail.com>
2016-03-11 18:16:03 +0800

    Initial Commit

---
+++ algo_hw1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algo_hw1
+//
+//  Created by Howard Yu on 2016/03/11.
+//  Copyright © 2016年 Howard Yu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
